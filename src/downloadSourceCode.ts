0e0329b96bc2f2588f79d1daab477d10c077e19e
marto97 <martinyankow@gmail.com>
2024-04-21 20:27:09 +0300

    feat: added check if the package exists, improve logging

--- src/downloadSourceCode.ts
+++ src/downloadSourceCode.ts
@@ -1,15 +1,18 @@
 const pacote = require('pacote');
 const path = require('path');
+const fs = require('fs');
 
 async function downloadPackageSource(packageName: any, targetDirectory: any) {
     try {
         const packageData = await pacote.manifest(packageName);
         const packageVersion = packageData.version;
         const packagePath = path.join(`${targetDirectory}/${packageName}`, `${packageName}@${packageVersion}`);
-
-        await pacote.extract(packageName, packagePath);
-
-        console.log(`Package "${packageName}" downloaded to ${packagePath}`);
+        if (fs.existsSync(packagePath)) {
+            console.log(`Package "${packageName}", version: ${packageVersion} is already downloaded to ${packagePath}`);
+        } else {
+            await pacote.extract(packageName, packagePath);
+            console.log(`Package "${packageName}" downloaded to ${packagePath}`);
+        }
     } catch (error) {
         console.error(`Error downloading package "${packageName}":`, error);
     }
@@ -17,7 +20,7 @@ async function downloadPackageSource(packageName: any, targetDirectory: any) {
 
 async function downloadAllPackagesSource(packageNames: any, targetDownloadDirectory: any) {
     for (let index = 0; index < packageNames.length; index++) {
-        console.log(`Started downloading source code for package: ${packageNames[index]}`);
+        console.log(`Started downloading ${index + 1}/${packageNames.length} source code for package: ${packageNames[index]}`);
         await downloadPackageSource(packageNames[index], targetDownloadDirectory);
     }
 }
