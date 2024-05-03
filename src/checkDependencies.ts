72fbb9581fd60b8ad7e1bd46673a265b66ad5da7
marto97 <martinyankow@gmail.com>
2024-05-03 11:37:28 +0300

    add check package.json dependencies

---
+++ src/checkDependencies.ts
@@ -0,0 +1,60 @@
+const axios = require('axios');
+const fs = require('fs');
+const path = require('path');
+
+interface DependencyInfo {
+    name: string;
+    version: string;
+}
+
+// TODO get it from local JSON file to reduce npm requests
+async function getLatestPackageVersion(packageName: string): Promise<string | null> {
+    try {
+        const response = await axios.get(`https://registry.npmjs.org/${packageName}`);
+        return response.data['dist-tags'].latest;
+    } catch (error) {
+        console.error(`Failed to fetch latest version for package '${packageName}': ${error}`);
+        return null;
+    }
+}
+
+function readPackageJsonDependencies(packageDir: string, dev: boolean): DependencyInfo[] | null {
+    try {
+        const packageJsonPath = path.join(packageDir, 'package.json');
+        const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf-8'));
+        const dependencies = dev ? packageJson.devDependencies : packageJson.dependencies;
+        if (!dependencies) {
+            return [];
+        }
+        return Object.entries(dependencies).map(([name, version]) => ({ name, version: version as string }));
+    } catch (error) {
+        console.error(`Error reading package.json in directory '${packageDir}': ${error}`);
+        return null;
+    }
+}
+
+async function checkDependencies(packageDir: string, dev: boolean): Promise<void> {
+    const dependencies = readPackageJsonDependencies(packageDir, dev);
+    if (!dependencies) {
+        console.error(`No valid dependencies found in package.json in directory '${packageDir}'`);
+        return;
+    }
+
+    for (const dependency of dependencies) {
+        const latestVersion = await getLatestPackageVersion(dependency.name);
+        if (!latestVersion) {
+            console.error(`Failed to get latest version for package '${dependency.name}'`);
+            continue;
+        }
+
+        if (dependency.version !== latestVersion) {
+            console.log(`Package '${dependency.name}' is outdated. Used version: ${dependency.version}, Latest version: ${latestVersion}`);
+        } else {
+            console.log(`Package '${dependency.name}' is up to date.`);
+        }
+    }
+}
+
+export{
+    checkDependencies
+}
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 import { downloadAllPackagesSource } from './downloadSourceCode';
 import { fetchAllPackageMetadata } from './fetchPackageMetadata';
+import { checkDependencies } from './checkDependencies';
+import { checkNpmPackage } from './findVulnerability';
 const packageNames = require("all-the-package-names");
 
 // Local testing for first 50 packages
 const allPackageNames = packageNames.slice(0, 50);
 const enableDownloadSourceCode = true;
 const enableDownloadMetaData = true;
+const enableCheckDependences = true;
 const targetDownloadDirectory = './data/downloaded_packages';
 const targetMetadataDirectory = './data/metadata';
 const localMetadataPackagesLatestVersion = './data/localPackagesLatestVersion.json'
@@ -18,6 +21,25 @@ async function main() {
         }
         if (enableDownloadSourceCode) {
             await downloadAllPackagesSource(allPackageNames, targetDownloadDirectory);
+        }
+        if (enableCheckDependences) {
+            const packageDir = './data/downloaded_packages/-adisagar2003-react-share-on-social/-adisagar2003-react-share-on-social@1.0.9'; // Replace with the path to your package directory
+            checkDependencies(packageDir, true); // For devDependencies
+            checkDependencies(packageDir, false); // For dependencies
+        }
+        if (true) {
+            const packageName = "react";
+            checkNpmPackage(packageName)
+                .then((advisory) => {
+                    if (advisory) {
+                        console.log("Advisory found for package:", advisory);
+                    } else {
+                        console.log("No advisory found for package.");
+                    }
+                })
+                .catch((error) => {
+                    console.error("Error:", error);
+                });
         }
     } catch (error) {
         console.error(`Failed to fetch or save package metadata: ${error}`);
