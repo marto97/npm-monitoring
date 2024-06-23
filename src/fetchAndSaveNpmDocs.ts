7d66c7a4921d581e3eeaf5ad489faaca52ce08ed
marto97 <martinyankow@gmail.com>
2024-06-23 03:01:08 +0200

    fixed saving big size metadata, up deps

--- src/fetchAndSaveNpmDocs.ts
+++ src/fetchAndSaveNpmDocs.ts
@@ -21,7 +21,7 @@ async function fetchAndSaveNpmDocsOld() {
     }
 }
 
-async function fetchAndSaveNpmDocs() {
+async function fetchAndSaveNpmDocsOld2() {
     let hasMore = true;
     let lastDocId = '';
     const fileName = 'npm_all_docs2.json';
@@ -66,6 +66,57 @@ async function fetchAndSaveNpmDocs() {
 
     // Close the JSON array properly
     appendFileSync(fileName, '{}]\n');  // Add an empty object to handle the trailing comma
+    console.log("All documents have been saved to npm_all_docs.json");
+}
+
+async function fetchAndSaveNpmDocs() {
+    let hasMore = true;
+    let lastDocId = '';
+    const fileName = 'npm_all_docs4.json';
+    writeFileSync(fileName, '[\n');  // Start the JSON array
+
+    console.time("Total time");
+
+    while (hasMore) {
+        console.time(`Step ${step + 1} time`);
+        try {
+            // Fetch a chunk of documents
+            const response = await axios.get(url, {
+                params: {
+                    include_docs: true,
+                    limit: limit,
+                    startkey: JSON.stringify(lastDocId)
+                }
+            });
+            const data = response.data;
+            
+            // Check if there are more documents
+            hasMore = data.rows.length === limit;
+
+            // Process and save the data
+            const docs = data.rows.map((row: any) => row.doc);
+            if (docs.length > 0) {
+                docs.forEach((doc: any, index: any) => {
+                    const jsonString = JSON.stringify(doc, null, 2);
+                    appendFileSync(fileName, jsonString + (hasMore || index < docs.length - 1 ? ',\n' : '\n'));
+                });
+                // Update the last document ID
+                lastDocId = docs[docs.length - 1]._id;
+            }
+
+            step = step + 1;
+
+            console.timeEnd(`Step ${step} time`);
+            console.log(`Fetched and saved ${docs.length} documents. Step: ${step}`);
+        } catch (error) {
+            console.error(`Failed to fetch data: ${error}`);
+            hasMore = false;
+        }
+    }
+
+    // Close the JSON array properly
+    appendFileSync(fileName, '{}]\n');  // Add an empty object to handle the trailing comma
+    console.timeEnd("Total time");
     console.log("All documents have been saved to npm_all_docs.json");
 }
 
