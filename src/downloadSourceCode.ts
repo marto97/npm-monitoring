5fed46ff051888dff4dee53d142f56d5151dfa80
marto97 <martinyankow@gmail.com>
2024-06-29 23:30:50 +0200

    reduce CONCURRENCY_LIMIT 16

--- src/downloadSourceCode.ts
+++ src/downloadSourceCode.ts
@@ -2,7 +2,7 @@ const pacote = require('pacote');
 const path = require('path');
 const fs = require('fs');
 
-const CONCURRENCY_LIMIT = 50; // Adjust this value based on server's capacity
+const CONCURRENCY_LIMIT = 16; // Adjust this value based on server's capacity
 
 // Helper function to get the current timestamp
 function getTimestamp(): string {
